cebe39b282a742c6f59c939d3dcd7816853bb110
ligligaggles <naween.ahsan@ryerson.ca>
2021-11-14 23:03:46 -0500

    Initial CNN model

---
+++ model.py
@@ -0,0 +1,118 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+K1 = [None, 3, 3, None, 6]
+K2 = [None, 3, 3, None, 5]
+P = [None, 1, 1, None, 2]
+
+class CNN:
+    def __init__(self, tm, W):
+        self.tm = tm
+        self.W = W
+        self.N = 7
+        self.t1 = 2 if 1 > tm else 4
+        self.t2 = 2 if 2 > tm else 4
+        self.t3 = 2 if 3 > tm else 4
+        self.t4 = 2 if 4 > tm else 4
+        self.model = None
+        self.initialize_layers()
+    
+    def initialize_layers(self):
+        inputs = keras.Input(shape=(128, None, 1), name="audio")
+
+        x = layers.BatchNormalization()(inputs)
+
+        # block 0
+        x = tfb_1(x, 1, 1, 8 * self.W)
+        x = tfb(x, 1, 1, 8 * self.W)
+        x = tfb(x, 1, 1, 8 * self.W)
+        x = tfb(x, 1, 1, 8 * self.W)
+
+        # block 1
+        x = tfb(x, 2, self.t1, 16 * self.W)
+        x = tfb(x, 1, 1, 16 * self.W)
+        x = tfb(x, 1, 1, 16 * self.W)
+        x = tfb(x, 1, 1, 16 * self.W)
+
+        # block 2
+        x = tfb(x, 2, self.t2, 32 * self.W)
+        x = tfb(x, 1, 1, 32 * self.W)
+        x = tfb(x, 1, 1, 32 * self.W)
+        x = tfb(x, 1, 1, 32 * self.W)
+
+        # block 3
+        x = tfb(x, 2, self.t3, 64 * self.W)
+        x = tfb(x, 1, 1, 64 * self.W)
+        x = tfb(x, 1, 1, 64 * self.W)
+        x = tfb(x, 1, 1, 64 * self.W)
+
+        # block 4
+        x = tfb(x, 2, self.t4, 128 * self.W)
+        x = tfb(x, 1, 1, 128 * self.W)
+        x = tfb(x, 1, 1, 128 * self.W)
+        x = tfb(x, 1, 1, 128 * self.W)
+
+        x = layers.GlobalAveragePooling2D()(x)
+        x = layers.Dense(128 * self.W, activation=layers.LeakyReLU(alpha=0.01))(x)
+        # x = layers.LeakyReLU(alpha=0.1)(x)
+
+        outputs = layers.Dense(self.N, activation="sigmoid")(x)
+
+        self.model = keras.Model(inputs, outputs, name='toy_cnn')
+
+        print(self.model.summary())
+
+
+def tfb(input, x, y, c):
+    print(input)
+
+    fx = layers.BatchNormalization()(input)
+    fx = layers.LeakyReLU(alpha=0.1)(fx)
+    fx = layers.ZeroPadding2D(padding=(1, 1))(fx)
+    fx = layers.Conv2D(c, (K1[x], K1[y]), (x, y), padding='valid')(fx)
+    fx = layers.BatchNormalization()(fx)
+    fx = layers.LeakyReLU(alpha=0.1)(fx)
+    fx = layers.ZeroPadding2D(padding=(P[x], P[y]))(fx)
+    fx = layers.Conv2D(c, (K2[x], K2[y]), (1, 1), padding='valid')(fx)
+
+    res = layers.ZeroPadding2D(padding=(0, 0))(input)
+    res = layers.Conv2D(c, (1, 1), (x, y), padding='valid')(res)
+
+    out_layers = []
+
+    if c == input.shape[2] and (x == 1 and y == 1):
+        out_layers.append(input)
+    
+    out_layers.append(fx)
+
+    if c != input.shape[2] or x != 1 or y != 1:
+        out_layers.append(res)
+
+    out = layers.Add()(out_layers)
+
+    return out
+
+def tfb_1(input, x, y, c):
+    fx = layers.ZeroPadding2D(padding=(1, 1))(input)
+    fx = layers.Conv2D(c, (K1[x], K1[y]), (x, y), padding='valid')(fx)
+    fx = layers.BatchNormalization()(fx)
+    fx = layers.LeakyReLU(alpha=0.1)(fx)
+    fx = layers.ZeroPadding2D(padding=(P[x], P[y]))(fx)
+    fx = layers.Conv2D(c, (K2[x], K2[y]), (1, 1), padding='valid')(fx)
+
+    res = layers.ZeroPadding2D(padding=(0, 0))(input)
+    res = layers.Conv2D(c, (1, 1), (x, y), padding='valid')(res)
+
+    out_layers = []
+
+    if c == input.shape[2] and (x == 1 and y == 1):
+        out_layers.append(input)
+    
+    out_layers.append(fx)
+
+    if c != input.shape[2] or x != 1 or y != 1:
+        out_layers.append(res)
+
+    out = layers.Add()(out_layers)
+
+    return out
--- testing.py
+++ testing.py
@@ -6,12 +6,22 @@ import matplotlib.pyplot as plt
 import utils.audio as audio
 import utils.dataset as dataset
 
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import model
+
 filename = 'datasets/RAVDESS/Actor_01/03-01-01-01-01-01-01.wav'
-dataset_path = 'datasets/RAVDESS/audio_speech_actors_01-24/'
+# filename = 'datasets/RAVDESS/Actor_01/03-01-03-01-01-02-01.wav'
+# dataset_path = 'datasets/RAVDESS/audio_speech_actors_01-24/'
 
-dataset.load_ravdess_dataset(dataset_path)
+# dataset.load_ravdess_dataset(dataset_path)
 
-# print(audio.convert_audio_to_log_mel_spectrogram(filename))
+print(audio.convert_audio_to_log_mel_spectrogram(filename))
 
 # plt.colorbar(format='%+2.0f dB')
 # plt.savefig('plot.png')
+
+# model.tfb()
+
+cnn = model.CNN(0, 4)
